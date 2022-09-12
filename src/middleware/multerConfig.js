5650fe6c1515981ab2cf3c2754c685069b3f4d59
Stéphanie R <korettex@gmail.com>
2022-09-12 16:18:03 +0200

    Create Middleware Multer File image

---
+++ src/middleware/multerConfig.js
@@ -0,0 +1,20 @@
+import multer from 'multer';
+
+const MIMETYPES = {
+    "image/jpg": "jpg",
+    "image/jpeg": "jpg",
+    "image/png": "png"
+};
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        const nameFile = file.originalname.split(' ').join('_');
+        const extFile = MIMETYPES[file.mimetype];
+        cb(null, `${nameFile}-${Date.now()}.${extFile}`);
+    }
+});
+
+export default multer({ storage }).single('image');
