d1e88a26ded2cd46fa1db5a9436e4ad5605064a7
roy-stephanie <korettex@gmail.com>
2022-09-27 11:14:17 +0200

    Refactoring CORS

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 // Middleware General
 app.use(express.json());
 app.use((req,res,next) => {
-    res.set('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.set('Access-Control-Allow-Origin', '*');
     res.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     res.set('Content-Type', 'application/json');
