cf8e8732cc4c2c4d513680fc79bb6de0462a7a6b
roy-stephanie <korettex@gmail.com>
2022-10-11 09:52:02 +0200

    Refactoring

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const fakeToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MzNiZTVkOWIzNTI4ZjQ1YzlkNDA1OGEiLCJoYWNrIjp0cnVlLCJpYXQiOjE2NjQ4NzA0MTQsImV4cCI6MTY2NDk1NjgxNH0.lch2OM_mdd95zrVHftuHNfrm5WD0jKXyQbKXrPUyzj0';
 // Middleware authenticator
 export const auth = (req, res, next) => {
     try {
@@ -11,6 +10,7 @@ export const auth = (req, res, next) => {
         const jwtToken = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(jwtToken, process.env.SECRETJWT);
         const userId = decodedToken.userId;
+        // Set UserID in Request Auth
         req.auth = {
             userId: userId
         };
@@ -18,20 +18,4 @@ export const auth = (req, res, next) => {
     } catch (error) {
         res.status(401).json({ error });
     }
-
-    /*const jwtToken = req.headers.authorization.split(' ')[1];
-
-    jwt.verify(fakeToken, process.env.SECRETJWT, (err, decodedToken) => {
-        if (err) {
-            //res.status(401).json({ err });
-
-            res.status(301).redirect('/login');
-        }
-        if (decodedToken) {
-            req.auth = {
-                userId: decodedToken.userId
-            };
-            next();
-        }
-    });*/
 }
